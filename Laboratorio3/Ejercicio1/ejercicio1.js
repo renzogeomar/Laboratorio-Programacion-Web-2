c5dce99e793e29e2331a271c4c3e25309ef7abd2
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-04-24 00:39:30 -0500

    terminando ejercicio 1

--- Laboratorio3/Ejercicio1/ejercicio1.js
+++ Laboratorio3/Ejercicio1/ejercicio1.js
@@ -2,6 +2,24 @@ let fechaActual= new Date();
 let day=fechaActual.getDay();
 console.log(fechaActual);
 console.log(day);
-function fecha(dia){
-
+console.log(fecha(day));
+function fecha(day){
+    switch(day){
+        case 0:
+            return "domingo";
+        case 1:
+            return "lunes";
+        case 2:
+            return "martes";
+        case 3:
+            return "miercoles";
+        case 4:
+            return "jueves";
+        case 5:
+            return "viernes";
+        case 6:
+            return "sabado";
+        default:
+            return "no valido";                           
+    }
 }

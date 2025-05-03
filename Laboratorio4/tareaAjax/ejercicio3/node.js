d0410cc5cc5f17982463a78b21675423f9824a04
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-02 23:15:31 -0500

    copiando el ejemplo 3 de wetschools y reutilizando el codigo de node.js del ejercicio anterior

---
+++ Laboratorio4/tareaAjax/ejercicio3/node.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Servir archivos estáticos desde el directorio actual
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
+});

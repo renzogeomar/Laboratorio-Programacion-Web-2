3040974f642b7f79e4e4a08eaa06624c9b652c1b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-02 22:46:59 -0500

    corrigiendo el codigo de node.js para que detecte ajax_info.txt

--- Laboratorio4/tareaAjax/ejercicio1/node.js
+++ Laboratorio4/tareaAjax/ejercicio1/node.js
@@ -2,11 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Servir archivos estáticos desde el directorio actual
+app.use(express.static(__dirname));
+
 app.listen(3000, () => {
-	console.log("Escuchando en: http://localhost:3000")
-
+    console.log('Servidor escuchando en http://localhost:3000');
 });
 
-app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'index.html'));
-});

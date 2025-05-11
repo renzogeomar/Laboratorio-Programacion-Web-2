b96fe29f3c1f41cf8b24d937b71bbd3c8d51d488
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-10 22:07:24 -0500

    iniciando el ejercicio2 'b'

---
+++ Laboratorio5/EjerciciosPropuestos/Ejercicio2/2b).py
@@ -0,0 +1,6 @@
+#Toda esta seccion es para poder usar los modulos fuera de esta carpeta
+import sys
+import os
+# Agrega la carpeta padre de Ejercicio2 al path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#.....

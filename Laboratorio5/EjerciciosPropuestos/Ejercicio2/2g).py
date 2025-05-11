5f57d3ca234b48d81c84995b544284a65c960108
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-10 23:03:09 -0500

    reutilizando el codigo del ejercicio anterior

--- Laboratorio5/EjerciciosPropuestos/Ejercicio2/2g).py
+++ Laboratorio5/EjerciciosPropuestos/Ejercicio2/2g).py
@@ -14,3 +14,25 @@ from chessPictures import square
 from chessPictures import queen
 from chessPictures import rock
 from interpreter import draw
+
+# Parte del tablero sin piezas
+black_square = square.negative()
+# Alternar: iniciando con negro
+fila1 = black_square
+for i in range(3):  # ya hay 1, agregamos 3 pares más
+    fila1 = fila1.join(square).join(black_square)
+# Falta una casilla negra al final para tener 8
+fila1 = fila1.join(square)
+
+# Alternar: iniciando con blanco
+fila2 = square
+for i in range(3):  # ya hay 1, agregamos 3 pares más
+    fila2 = fila2.join(black_square).join(square)
+# Falta una casilla negra al final para tener 8
+fila2 = fila2.join(black_square)
+tableroSinPiezas = fila2.up(fila1).up(fila2).up(fila1)
+#------------------------------------------------------------
+
+# Parte del tablero con piezas
+parteConPiezas = fila1.up(fila2)
+draw(parteConPiezas)

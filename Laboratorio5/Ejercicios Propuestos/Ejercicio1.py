ca4c97b2dd4114ce3cb04fedfb304c0b118c60ef
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-10 21:08:15 -0500

    haciendo el ejercicio1, este prueba los metodos con 'img' que es un ejemplo, aun no se aplica con las piezas

---
+++ Laboratorio5/Ejercicios Propuestos/Ejercicio1.py
@@ -0,0 +1,49 @@
+from picture import Picture
+img = [
+    "abc",
+    "def",
+    "ghi"
+]
+
+pic = Picture(img)
+def mostrar(picture):
+    for fila in picture.img:
+        print(fila)
+    print()  # línea en blanco para separar
+
+print("Original:")
+mostrar(pic)
+
+print("Espejo vertical:")
+mostrar(pic.verticalMirror())
+
+print("Espejo horizontal:")
+mostrar(pic.horizontalMirror())
+
+print("Negative:")
+# para esta prueba, asegúrate de tener inverter definido en colors.py con los caracteres usados.
+mostrar(pic.negative())
+
+print("Unirse con uno mismmo/ Join with itself:")
+mostrar(pic.join(pic))
+
+print("Arriba con uno mismo /Up with itself:")
+mostrar(pic.up(pic))
+
+print("Debajo (con anulación de espacio) / Under (with space override):")
+# una figura con espacio para ver el efecto
+mask = Picture([
+    " A ",
+    "   ",
+    " C "
+])
+mostrar(pic.under(mask))
+
+print("Horizontal Repeat (x2):")
+mostrar(pic.horizontalRepeat(4))
+
+print("Vertical Repeat (x2):")
+mostrar(pic.verticalRepeat(4))
+
+print("Rotate 90° clockwise:")
+mostrar(pic.rotate())

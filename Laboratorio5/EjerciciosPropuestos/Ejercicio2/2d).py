2d6d8c3bd9f0ecf9d13dbd7a7ae0811061d0cf90
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-10 22:43:07 -0500

    terminando el ejercicio2 'd'

--- Laboratorio5/EjerciciosPropuestos/Ejercicio2/2d).py
+++ Laboratorio5/EjerciciosPropuestos/Ejercicio2/2d).py
@@ -4,3 +4,16 @@ import os
 # Agrega la carpeta padre de Ejercicio2 al path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 #..... 
+
+from chessPictures import square
+from interpreter import draw
+
+black_square = square.negative()
+
+# Alternar: iniciando con blanco
+fila = square
+for i in range(3):  # ya hay 1, agregamos 3 pares más
+    fila = fila.join(black_square).join(square)
+# Falta una casilla negra al final para tener 8
+fila = fila.join(black_square)
+draw(fila)

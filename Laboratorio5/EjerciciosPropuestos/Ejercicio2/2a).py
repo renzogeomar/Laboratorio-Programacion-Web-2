50c2eb8690cffc86c6b14f16a621d898cc2f038b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-10 21:50:57 -0500

    probando que pueda usar los metodos dentro de la carpeta Ejercicio2

--- Laboratorio5/EjerciciosPropuestos/Ejercicio2/2a).py
+++ Laboratorio5/EjerciciosPropuestos/Ejercicio2/2a).py
@@ -1,12 +1,13 @@
+#Toda esta seccion es para poder usar los modulos fuera de esta carpeta
 import sys
 import os
-
 # Agrega la carpeta padre de Ejercicio2 al path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#.....
 
-from chessPictures import rock
+from chessPictures import knight
 from interpreter import draw
 
 # unir dos torres
-figura = rock.join(rock)
+figura = knight.join(knight)
 draw(figura)
--- Laboratorio5/EjerciciosPropuestos/prueba.py
+++
@@ -1,7 +0,0 @@
-from interpreter import draw
-from chessPictures import rock
-
-# Ejercicio 2.a: unir dos torres (rock) horizontalmente
-figura = rock.join(rock)
-
-draw(figura)

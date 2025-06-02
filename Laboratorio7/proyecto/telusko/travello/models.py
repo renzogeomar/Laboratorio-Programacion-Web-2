f3afd8d9a4da7c966673d22ddb405b3a2c8c2f17
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-02 00:15:14 -0500

    haciendo el modelo para la tabla

--- Laboratorio7/proyecto/telusko/travello/models.py
+++ Laboratorio7/proyecto/telusko/travello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='imagenes_destinos/')
+    precioTour = models.DecimalField(max_digits=8, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombreCiudad

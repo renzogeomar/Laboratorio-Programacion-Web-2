99ba310bb7d29bdf92d46a50ecfa543d07f5ed92
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-02 00:32:40 -0500

    agregando una pagina que le permita al usuario ver sus destinos turisticos

--- Laboratorio7/proyecto/telusko/telusko/settings.py
+++ Laboratorio7/proyecto/telusko/telusko/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travello',  # Your custom app for travel destinations
 ]
 
 MIDDLEWARE = [
--- Laboratorio7/proyecto/telusko/travello/views.py
+++ Laboratorio7/proyecto/telusko/travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import DestinosTuristicos
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def destinos(request):
+    destinos = DestinosTuristicos.objects.all()
+    return render(request, 'destinos.html', {'destinos': destinos})

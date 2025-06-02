902a1c1e8ee76458fefc2d70ea43cc977a396698
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-01 23:18:58 -0500

    probando con un html estatico

--- Laboratorio7/proyecto/telusko/calc/views.py
+++ Laboratorio7/proyecto/telusko/calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse as HTTPResponse
 
 # Create your views here.
 def home(request):
-    return HTTPResponse("Hello, Telusko!")
+    return render(request, 'home.html')

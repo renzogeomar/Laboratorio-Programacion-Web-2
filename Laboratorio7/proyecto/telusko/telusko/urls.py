d3880dc23ee78f3e4559e2e0088d544d666d1035
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-01 23:59:50 -0500

    copiando todas las plantillas y configurando para que se muestre correctamente

--- Laboratorio7/proyecto/telusko/telusko/urls.py
+++ Laboratorio7/proyecto/telusko/telusko/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('', include('calc.urls')),  # Include the URLs from the calc app
+    path('', include('travello.urls')),  
     path('admin/', admin.site.urls),
 ]

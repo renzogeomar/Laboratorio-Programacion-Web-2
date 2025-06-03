47c6f5be70a0c3cad4c029d271b6a0b74f50ac46
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-02 20:04:09 -0500

    creando las vistas y registrandolas en urls

--- Laboratorio7/proyecto/telusko/travello/urls.py
+++ Laboratorio7/proyecto/telusko/travello/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('destinos/', views.destinos, name='destinos'),
-
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
--- Laboratorio7/proyecto/telusko/travello/views.py
+++ Laboratorio7/proyecto/telusko/travello/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import DestinosTuristicos
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +12,33 @@ def index(request):
 def destinos(request):
     destinos = DestinosTuristicos.objects.all()
     return render(request, 'destinos.html', {'destinos': destinos})
+def registro(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El usuario ya existe.")
+            return redirect('registro')
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        messages.success(request, "Usuario registrado correctamente.")
+        return redirect('login')
+    return render(request, 'registro.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos.")
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')

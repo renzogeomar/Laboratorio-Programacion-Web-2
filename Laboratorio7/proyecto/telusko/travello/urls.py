cb0d6c8f2f02b31de1d3497fd05f2102ec616205
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-02 21:55:59 -0500

    añadiendo la logica para listar y para elimniar los lugares que ya se crearon, además de incluir sus direcciones en url,py

--- Laboratorio7/proyecto/telusko/travello/urls.py
+++ Laboratorio7/proyecto/telusko/travello/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('destinos/', views.destinos, name='destinos'),
+    #path('destinos/', views.destinos, name='destinos'),
     path('registro/', views.registro, name='registro'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    path('destinos/', views.listar_destinos, name='listar_destinos'),
+    path('destinos/nuevo/', views.crear_destino, name='crear_destino'),
+    path('destinos/editar/<int:id>/', views.modificar_destino, name='modificar_destino'),
+    path('destinos/eliminar/<int:id>/', views.eliminar_destino, name='eliminar_destino'),
 ]
 

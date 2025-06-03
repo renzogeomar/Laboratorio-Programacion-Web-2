c8d041bc4ab73b814376645e49cd7d9eb8aaaf4d
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-02 20:37:47 -0500

    corrigiendo el registro y su template

--- Laboratorio7/proyecto/telusko/travello/views.py
+++ Laboratorio7/proyecto/telusko/travello/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import RegistroUsuarioForm
 
 # Create your views here.
 def index(request):
@@ -14,17 +14,14 @@ def destinos(request):
     return render(request, 'destinos.html', {'destinos': destinos})
 def registro(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "El usuario ya existe.")
-            return redirect('registro')
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-        messages.success(request, "Usuario registrado correctamente.")
-        return redirect('login')
-    return render(request, 'registro.html')
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect('index')
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'registro.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':

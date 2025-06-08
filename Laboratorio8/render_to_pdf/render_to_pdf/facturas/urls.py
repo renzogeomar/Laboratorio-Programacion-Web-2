08351b52ef93ddc64fb307bb2d4e925c02682c93
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-08 18:06:01 -0500

    creando una funcion para generar un PDF dinámico de una factura

--- Laboratorio8/render_to_pdf/render_to_pdf/facturas/urls.py
+++ Laboratorio8/render_to_pdf/render_to_pdf/facturas/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import pdf_view
+from .views import advanced_pdf_view
+
 urlpatterns = [
     path('factura/', pdf_view, name='ver_factura_pdf'),
+    path('factura-avanzada/', advanced_pdf_view, name='ver_factura_pdf_avanzada'),
 ]
--- Laboratorio8/render_to_pdf/render_to_pdf/facturas/views.py
+++ Laboratorio8/render_to_pdf/render_to_pdf/facturas/views.py
@@ -2,6 +2,9 @@ import datetime
 from django.http import Http404
 from django.shortcuts import render
 from render_to_pdf.renderers import render_to_pdf 
+import locale
+locale.setlocale(locale.LC_ALL, "")
+
 # Create your views here.
 def pdf_view(request, *args, **kwargs):
     data = {
@@ -12,3 +15,29 @@ def pdf_view(request, *args, **kwargs):
         'pdf_title': 'Factura PDF'
     }
     return render_to_pdf('pdftemplates/pdfsinvoice.html', data)
+
+def advanced_pdf_view(request):
+    invoice_number = "007cae"
+    context = {
+        "customer_name": "Ethan Hunt",
+        "invoice_number": f"{invoice_number}",
+        "amount": locale.currency(100_000, grouping=True),
+        "date": "2021-07-04",
+        "pdf_title": f"Invoice #{invoice_number}",
+    }
+    response = render_to_pdf("pdftemplates/pdfsinvoice.html", context)
+
+    if response.status_code == 404:
+        raise Http404("Invoice not found")
+
+    filename = f"Invoice_{invoice_number}.pdf"
+
+    # Por defecto se mostrará en el navegador
+    content = f"inline; filename={filename}"
+
+    # Si se pasa el parámetro ?download=1, se forzará la descarga
+    if request.GET.get("download"):
+        content = f"attachment; filename={filename}"
+
+    response["Content-Disposition"] = content
+    return response

2f063025f460cae1c65be77b72d7608fd18aa807
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-07 22:32:48 -0500

    creando los modelos y registrandolos

--- Laboratorio8/miWeb/example/models.py
+++ Laboratorio8/miWeb/example/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Simple(models.Model):
+    text = models.CharField(max_length = 10)
+    number = models.IntegerField(null = True)
+    url = models.URLField(default = 'www.example.com') 
+    
+    def __str__(self):
+        return self.url 
+
+class DateExample(models.Model):
+    the_date = models.DateTimeField()
+
+class NullExample(models.Model):
+    col = models.CharField(max_length=10 , blank=True, null=True)
+
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+
+class FrameWork(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Language, on_delete= models.CASCADE)

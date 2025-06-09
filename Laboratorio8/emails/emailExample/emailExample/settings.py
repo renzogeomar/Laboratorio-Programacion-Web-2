7e67a561b5b5a8c6ecf01f4f9118afcd9f1c12e8
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-08 21:06:05 -0500

    añadiendo la configuracion para determinar el tipo de host, el correo, el puerto y más añadidos

--- Laboratorio8/emails/emailExample/emailExample/settings.py
+++ Laboratorio8/emails/emailExample/emailExample/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'renzoCorreoPrueba@outlook.com'
+EMAIL_HOST_PASSWORD = '123ABC456'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
